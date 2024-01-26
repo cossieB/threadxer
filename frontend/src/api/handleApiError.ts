b2aeb6acb8d0b6f9bb7ef8d29c72e3126a181998
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2024-01-26 17:44:44 +0200

    fix fetch to remove duplicate refresh requests

--- frontend/src/api/handleApiError.ts
+++ frontend/src/api/handleApiError.ts
@@ -1,4 +1,4 @@
-export async function handleApiError(res: Response) {console.log("HERE")
+export async function handleApiError(res: Response) {
     if (res.headers.get('Content-Type')?.includes('application/json')) {
         const data = await res.json();
         return new Error(data.error);
--- frontend/src/utils/customFetcher.ts
+++ frontend/src/utils/customFetcher.ts
@@ -3,27 +3,26 @@ import auth from "~/globalState/auth"
 type U = Parameters<typeof fetch>[0]
 type V = Parameters<typeof fetch>[1]
 
-export async function customFetch(url: U, reqOpts?: V) {
-    let isStale = (auth.token.decoded()?.exp ?? 0 ) * 1000 < new Date().getTime() + 60000 
-    
-    // Don't attempt to get the refresh token if there is no user logged in.
-    if (!auth.user.username) {
-        isStale = false
-    }
-        
-    if (isStale) {
+let canRefresh = true;
+
+export async function customFetch(url: U, requestOpts?: V) {
+    let isStale = Boolean(auth.user.username) && (auth.token.decoded()?.exp ?? 0) * 1000 < new Date().getTime() + 60000
+
+    if (isStale && canRefresh) {
+        canRefresh = false
         await refresh()
+        canRefresh = true
     }
     return fetch(url, {
-        ...reqOpts,
+        ...requestOpts,
         headers: {
-            ...(reqOpts?.headers ?? {}),
+            ...(requestOpts?.headers ?? {}),
             ...auth.token.jwt && ({ Authorization: `Bearer ${auth.token.jwt}` })
         }
     })
 }
 
-export async function refresh() {
+async function refresh() {
     const result = await fetch('/api/auth/refresh')
     if (result.status == 401 || result.status == 403) {
         return auth.deleteUser()
