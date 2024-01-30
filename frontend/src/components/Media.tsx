d4f8676ff40f12b3b5d7193c59085703c818e7c4
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2024-01-30 18:25:07 +0200

    Media slideshow

--- frontend/src/components/Media.tsx
+++ frontend/src/components/Media.tsx
@@ -1,19 +1,45 @@
-import { For, Show, createSignal } from "solid-js";
+import { For, Show, createSignal, mergeProps } from "solid-js";
 import { Portal } from "solid-js/web";
 import { PostResponse } from "~/api/postFetchers";
+import clickOutside from "~/lib/clickOutside";
 import styles from "~/styles/components/Media.module.scss"
+false && clickOutside
 
 type P = { media: NonNullable<PostResponse['media']> }
 
 export function MediaList(props: P) {
-    const [showSlideshow, setShowSlideshow] = createSignal(false)
+    const [showSlideshow, setShowSlideshow] = createSignal(false);
+    const [index, setIndex] = createSignal(0)
+
+    const openSlideshow = (i: number) => {
+        setShowSlideshow(true);
+        setIndex(i)
+    }
+    const changeSlide = (num: number) => setIndex(prev => prev + num)
+
     return (
         <div class={styles.container}>
             <For each={props.media}>
-                {m => <Media m={m} />}
+                {(m, i) => (
+                    <>
+                        <Media
+                            openSlideshow={() => openSlideshow(i())}
+                            m={m}
+                            i={i()}
+                        />
+                    </>
+                )}
             </For>
             <Show when={showSlideshow()}>
-                {/* <Slideshow i={} /> */}
+                <Slideshow
+                    media={props.media}
+                    i={index()}
+                    close={() => {
+                        setShowSlideshow(false)
+                        setIndex(0)
+                    }}
+                    changeSlide={changeSlide}
+                />
             </Show>
         </div>
     )
@@ -23,30 +49,45 @@ type P1 = {
     m: {
         url: string;
         is_video: boolean;
-    }
+    },
+    i?: number
+    openSlideshow?: (i: number) => void
 }
 
 function Media(props: P1) {
+    const merged = mergeProps(props, { i: 0 })
     return (
         <Show
             when={props.m.is_video}
-            fallback={<img src={props.m.url} />}
-        >
+            fallback={
+                <img onclick={() => props.openSlideshow?.(merged.i)} src={props.m.url} />
+            }>
             <video
                 src={props.m.url}
                 controls
+                onclick={() => props.openSlideshow?.(merged.i)}
             />
         </Show>
     )
 }
 
-function Slideshow(props: P & { i?: number }) {
+type P2 = {
+    i: number,
+    close(): void
+    changeSlide(num: number): void
+} & P
+
+function Slideshow(props: P2) {
     return (
-        <Portal>
-            <div class={styles.slideshow} >
-                <For each={props.media}>
-                    {m => <Media m={m} />}
-                </For>
+        <Portal ref={el => el.classList.add('modal')}>
+            <div class={styles.slideshow} use:clickOutside={props.close}>
+                <button disabled={props.i == 0} class={styles.prevBtn} onclick={() => props.changeSlide(-1)}>
+                    PREV
+                </button>
+                <Media m={props.media[props.i]} />
+                <button disabled={props.i == props.media.length - 1} class={styles.nextBtn} onclick={() => props.changeSlide(1)}>
+                    NEXT
+                </button>
             </div>
         </Portal>
     )
