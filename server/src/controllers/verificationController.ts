63d7cef7a7a94a44e906c130a6ed34a2b40294f9
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2024-02-10 16:08:03 +0200

    port verification routes to trpc

--- server/src/controllers/verificationController.ts
+++ server/src/controllers/verificationController.ts
@@ -7,100 +7,11 @@ import { RefreshTokens, User, VerificationCodes } from "../db/schema";
 import { handleTokens } from "../utils/generateCookies";
 import { eq, sql } from "drizzle-orm";
 import { draftVerificationEmail } from "../utils/draftEmail";
-import { redis } from "../utils/redis";
 
 export async function verifyUser(req: Request, res: Response, next: NextFunction) {
-    const oldAccessToken = res.locals.token!;
-    if (!oldAccessToken.user.isUnverified)
-        throw new TRPCError({code: 'BAD_REQUEST', message: "Already verified"})
-    let storedCode: string;
-    const cachedCode = await redis.get(`verification:${oldAccessToken.user.userId}`)
-    if (cachedCode)
-        storedCode = cachedCode
-    else {
-        const row = await db.query.VerificationCodes.findFirst({
-            where(fields, operators) {
-                return operators.eq(fields.userId, oldAccessToken.user.userId)
-            }
-        })
-        if (!row)
-            return next(new AppError('Error. Please click "resend".', 400))
-        if (row.expiry < new Date) {
-            const code = randomInt(999999).toString().padStart(6, '0');
-            await Promise.all([
-                db
-                    .update(VerificationCodes)
-                    .set({
-                        code,
-                        expiry: sql`NOW() + INTERVAL '72 hours'`
-                    })
-                    .where(eq(VerificationCodes.userId, oldAccessToken.user.userId)),
-                redis.setex(`verification:${oldAccessToken.user.userId}`, 259200, code)
-            ])
-            draftVerificationEmail(oldAccessToken.user.username, code, oldAccessToken.user.email)
-            throw new TRPCError({code: 'BAD_REQUEST', message: "Code expired. Check your email for a new code."})
-        }
-        await redis.setex(`verification:${oldAccessToken.user.userId}`, 259200, row.code)
-        storedCode = row.code
-    }
-    try {
-        if (storedCode !== req.body.code)
-            return next(new AppError('Invalid Code', 400))
-        const now = new Date;
-        await db.transaction(async tx => {
-            await tx.update(User)
-                .set({
-                    emailVerified: now
-                })
-                .where(eq(User.userId, oldAccessToken.user.userId))
-            await tx.update(VerificationCodes)
-                .set({
-                    dateUsed: now
-                })
-            await tx.delete(RefreshTokens).where(eq(RefreshTokens.token, req.cookies.rf))
-        })
-        const { accessToken: newAccessToken, cookie, fb } = await handleTokens({ ...oldAccessToken.user, isUnverified: false }, async refreshToken => {
-            await db.transaction(async tx => {
-                await tx.delete(RefreshTokens).where(eq(RefreshTokens.token, req.cookies.rf))
-                await tx.insert(RefreshTokens).values({
-                    token: refreshToken,
-                    userId: oldAccessToken.user.userId
-                })
-            })
-        })
-        res.setHeader('Set-Cookie', cookie)
-        return res.json({ jwt: newAccessToken, fb })
-    } catch (error) {
-        throw new TRPCError({code: 'INTERNAL_SERVER_ERROR', message: "Something went wrong. Please try again later."})
-    }
+   
 }
 
 export async function resendVerificationToken(req: Request, res: Response, next: NextFunction) {
-    const code = randomInt(999999).toString().padStart(6, '0');
-    const accessToken = res.locals.token!;
-    if (!accessToken.user.isUnverified)
-        return next(new AppError('Already Verified', 400))
-    try {
-        await db.insert(VerificationCodes)
-            .values({
-                userId: accessToken.user.userId,
-                code,
-            })
-            .onConflictDoUpdate({
-                target: VerificationCodes.userId,
-                set: {
-                    code,
-                    expiry: sql`NOW() + INTERVAL '72 hours'`
-                }
-            })
-        draftVerificationEmail(accessToken.user.username, code, accessToken.user.email)
-        res.sendStatus(200)
-    }
-    catch (error) {
-        if (error instanceof PostgresError) {
-            if (error.message.includes("violates foreign key constraint"))
-                throw new TRPCError({code: 'BAD_REQUEST', message: "User Not Found"})
-        }
-        throw new TRPCError({code: 'INTERNAL_SERVER_ERROR', message: "Something went wrong. Please try again later."})
-    }
+
 }
--- server/src/routers/verify.ts
+++ server/src/routers/verify.ts
@@ -1,10 +1,114 @@
-import express from 'express'
-import { authorize } from '../middleware/authenticate'
-import { validation } from '../middleware/validation'
-import * as verificationController from '../controllers/verificationController'
+import { randomInt } from 'crypto'
+import { sql, eq } from 'drizzle-orm'
+import { db } from '../db/drizzle'
+import { VerificationCodes, User, RefreshTokens } from '../db/schema'
+import { redis } from '../redis'
+import { protectedProcedure, router } from '../trpc'
+import { TRPCError } from '@trpc/server'
+import { draftVerificationEmail } from '../utils/draftEmail'
+import { handleTokens } from '../utils/generateCookies'
+import { z } from 'zod'
+import { PostgresError } from 'postgres'
 
-export const verificationRouter = express.Router()
-
-verificationRouter.post('/', authorize, validation(['code']), verificationController.verifyUser)
-
-verificationRouter.get('/', authorize, verificationController.resendVerificationToken)
+export const verificationRouter = router({
+    verifyUser: protectedProcedure
+        .input(z.object({
+            code: z.string(),
+        }))
+        .mutation(async ({ ctx, input }) => {
+            if (!ctx.user.isUnverified)
+                throw new TRPCError({ code: 'BAD_REQUEST', message: "Already verified" })
+            let storedCode: string;
+            const cachedCode = await redis.get(`verification:${ctx.user.userId}`)
+            if (cachedCode)
+                storedCode = cachedCode
+            else {
+                const row = await db.query.VerificationCodes.findFirst({
+                    where(fields, operators) {
+                        return operators.eq(fields.userId, ctx.user.userId)
+                    }
+                })
+                if (!row)
+                    throw new TRPCError({ code: 'BAD_REQUEST', message: 'Error. Please click "resend".' })
+                if (row.expiry < new Date) {
+                    const code = randomInt(999999).toString().padStart(6, '0');
+                    await Promise.all([
+                        db
+                            .update(VerificationCodes)
+                            .set({
+                                code,
+                                expiry: sql`NOW() + INTERVAL '72 hours'`
+                            })
+                            .where(eq(VerificationCodes.userId, ctx.user.userId)),
+                        redis.setex(`verification:${ctx.user.userId}`, 259200, code)
+                    ])
+                    draftVerificationEmail(ctx.user.username, code, ctx.user.email)
+                    throw new TRPCError({ code: 'BAD_REQUEST', message: "Code expired. Check your email for a new code." })
+                }
+                await redis.setex(`verification:${ctx.user.userId}`, 259200, row.code)
+                storedCode = row.code
+            }
+            try {
+                if (storedCode !== input.code)
+                    throw new TRPCError({ code: 'BAD_REQUEST', message: 'Invalid Code' })
+                const now = new Date;
+                await db.transaction(async tx => {
+                    await tx.update(User)
+                        .set({
+                            emailVerified: now
+                        })
+                        .where(eq(User.userId, ctx.user.userId))
+                    await tx.update(VerificationCodes)
+                        .set({
+                            dateUsed: now
+                        })
+                    if (ctx.req.cookies.rf)
+                        await tx.delete(RefreshTokens).where(eq(RefreshTokens.token, ctx.req.cookies.rf))
+                })
+                const { accessToken: newAccessToken, cookie, fb } = await handleTokens({ ...ctx.user, isUnverified: false }, async refreshToken => {
+                    await db.transaction(async tx => {
+                        if (ctx.req.cookies.rf)
+                            await tx.delete(RefreshTokens).where(eq(RefreshTokens.token, ctx.req.cookies.rf))
+                        await tx.insert(RefreshTokens).values({
+                            token: refreshToken,
+                            userId: ctx.user.userId
+                        })
+                    })
+                })
+                ctx.res.header('Set-Cookie', cookie)
+                return { jwt: newAccessToken, fb }
+            } catch (error) {
+                throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: "Something went wrong. Please try again later." })
+            }
+        }),
+    
+    resendToken: protectedProcedure
+        .mutation(async ({ctx}) => {
+            const code = randomInt(999999).toString().padStart(6, '0');
+            if (!ctx.user.isUnverified)
+                throw new TRPCError({code: 'BAD_REQUEST', message: 'Already Verified'})
+            try {
+                await db.insert(VerificationCodes)
+                    .values({
+                        userId: ctx.user.userId,
+                        code,
+                    })
+                    .onConflictDoUpdate({
+                        target: VerificationCodes.userId,
+                        set: {
+                            code,
+                            expiry: sql`NOW() + INTERVAL '72 hours'`
+                        }
+                    })
+                draftVerificationEmail(ctx.user.username, code, ctx.user.email)
+                return
+            }
+            catch (error) {
+                if (error instanceof PostgresError) {
+                    if (error.message.includes("violates foreign key constraint"))
+                        throw new TRPCError({code: 'BAD_REQUEST', message: "User Not Found"})
+                }
+                throw new TRPCError({code: 'INTERNAL_SERVER_ERROR', message: "Something went wrong. Please try again later."})
+            }
+        })
+})
